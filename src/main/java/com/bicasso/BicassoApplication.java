3445c158dd1fb5ae83520fdd5c112907e9ad2ab3
zhaoyanbing@shhxzq.com <@q1w2e3r4>
2017-08-23 15:07:49 +0800

    init

---
+++ src/main/java/com/bicasso/BicassoApplication.java
@@ -0,0 +1,12 @@
+package com.bicasso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BicassoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BicassoApplication.class, args);
+	}
+}
